439e180c700d3380579d2b4159797d659b17fcec
Ralph Bean <rbean@redhat.com>
2023-02-13 17:36:18 -0500

    Update tests for changes to data model

--- tests/test_main.py
+++ tests/test_main.py
@@ -3,13 +3,36 @@ import unittest.mock
 import sprintreport
 
 
-class MockRawIssue(object):
-    def __init__(self, key, summary, rank, epic=None, feature=None):
-        self.key = key
+class MockStatus(object):
+    def __init__(self, name):
+        self.raw = {
+            'statusCategory': {
+                'name': name,
+            },
+        }
+
+
+class MockFields(object):
+    def __init__(self, summary, rank, assignee, status, epic, feature):
         self.summary = summary
-        self.rank = rank
-        self.epic = epic
-        self.feature = feature
+        self.customfield_12311940 = rank
+        self.assignee = assignee
+        self.status = status
+        self.customfield_12311140 = epic
+        self.customfield_12313140 = feature
+
+
+class MockRawIssue(object):
+    def __init__(self, key, summary, rank, assignee='nobody', epic=None, feature=None):
+        self.key = key
+        self.fields = MockFields(
+            summary,
+            rank,
+            assignee,
+            status=MockStatus('nothing'),
+            epic=epic,
+            feature=feature,
+        )
 
 
 def _mock_client(self):
@@ -40,7 +63,7 @@ def test_gather_simple():
 def test_gather_one_epic():
     mock_queries = {
         'x and type not in (Feature, Epic) and (statusCategory != Done or resolutionDate > x)': [
-            MockRawIssue("FOO-123", "Simple issue", "abcdef", "FOO-500"),
+            MockRawIssue("FOO-123", "Simple issue", "abcdef", epic="FOO-500"),
         ],
         'key=FOO-500': [
             MockRawIssue("FOO-500", "Simple epic", "abcdef"),
@@ -65,10 +88,12 @@ def test_gather_one_epic():
 def test_gather_one_feature():
     mock_queries = {
         'x and type not in (Feature, Epic) and (statusCategory != Done or resolutionDate > x)': [
-            MockRawIssue("FOO-123", "Simple issue", "abcdef", "FOO-500"),
+            MockRawIssue("FOO-123", "Simple issue", "abcdef", epic="FOO-500"),
         ],
         'key=FOO-500': [
-            MockRawIssue("FOO-500", "Simple epic", "abcdef", None, "FOO-9000"),
+            MockRawIssue(
+                "FOO-500", "Simple epic", "abcdef", epic=None, feature="FOO-9000"
+            ),
         ],
         'key=FOO-9000': [
             MockRawIssue("FOO-9000", "Simple feature", "abcdef"),
@@ -95,11 +120,13 @@ def test_gather_one_feature():
 def test_gather_one_extra_orphan_issue():
     mock_queries = {
         'x and type not in (Feature, Epic) and (statusCategory != Done or resolutionDate > x)': [
-            MockRawIssue("FOO-123", "Simple issue", "abcdef", "FOO-500"),
+            MockRawIssue("FOO-123", "Simple issue", "abcdef", epic="FOO-500"),
             MockRawIssue("FOO-124", "Orphan issue", "abcdef"),
         ],
         'key=FOO-500': [
-            MockRawIssue("FOO-500", "Simple epic", "abcdef", None, "FOO-9000"),
+            MockRawIssue(
+                "FOO-500", "Simple epic", "abcdef", epic=None, feature="FOO-9000"
+            ),
         ],
         'key=FOO-9000': [
             MockRawIssue("FOO-9000", "Simple feature", "abcdef"),
@@ -127,11 +154,13 @@ def test_gather_one_extra_orphan_issue():
 def test_gather_one_extra_orphan_epic():
     mock_queries = {
         'x and type not in (Feature, Epic) and (statusCategory != Done or resolutionDate > x)': [
-            MockRawIssue("FOO-123", "Simple issue", "abcdef", "FOO-500"),
-            MockRawIssue("FOO-124", "Kinda orphan issue", "abcdef", "FOO-501"),
+            MockRawIssue("FOO-123", "Simple issue", "abcdef", epic="FOO-500"),
+            MockRawIssue("FOO-124", "Kinda orphan issue", "abcdef", epic="FOO-501"),
         ],
         'key=FOO-500': [
-            MockRawIssue("FOO-500", "Simple epic", "abcdef", None, "FOO-9000")
+            MockRawIssue(
+                "FOO-500", "Simple epic", "abcdef", epic=None, feature="FOO-9000"
+            )
         ],
         'key=FOO-501': [MockRawIssue("FOO-501", "Orphan epic", "abcdef")],
         'key=FOO-9000': [MockRawIssue("FOO-9000", "Simple feature", "abcdef")],
